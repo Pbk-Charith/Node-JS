41b2cf9272b99b018a535d836175eda31cefdfbe
Pbk-Charith <pbcharith01@gmail.com>
2025-11-09 01:05:07 +0530

    Refactor app and sum modules; add data.json and package.json files

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 require('./xyz');
-const {x, calculateSum} = require('./sum');
-
-var name = "charith";
+const {calculateSum} = require('./sum');
+const data = require('./data.json');
+calculateSum(a, b);
+console.log(data);
 
 var a = 10;
 var b = 20;
 
 // var sum = a + b;
 
-calculateSum(a, b);
-console.log(x);
-
 // console.log("Sum of " + a + " and " + b + " is: " + sum);
+var name = "charith";
 console.log("Name is: " + name);
--- sum.js
+++ sum.js
@@ -1,16 +1,14 @@
 // #Module protects their variables and functions from leaking into global scope
 
 console.log("Sum module loaded");
-var x = "hello World";
 const calculateSum = (a, b) => {
     const sum = a + b;
     console.log("Sum of " + a + " and " + b + " is: " + sum);
 }
 
-module.exports = {x, calculateSum};
+module.exports = {calculateSum};
 
 // console.log(module.exports);
 
 // you can use module exports like this also
-module.exports.x = x;
 module.exports.calculateSum = calculateSum;    
