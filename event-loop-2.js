b0c4664815acefde05ecc70649973e90d7c80004
Pbk-Charith <pbcharith01@gmail.com>
2026-02-17 17:54:16 +0530

    All About event loop

--- event-loop-2.js
+++ event-loop-2.js
@@ -4,15 +4,25 @@ const a = 100;
 
 setImmediate(() => console.log("setImmediate"));
 
-Promise.resolve().then(() => console.log("Promise"));
+Promise.resolve().then(() => console.log("Promise1"));
+
+process.nextTick(() => console.log("process.nextTick1"));
 
 fs.readFile("./file.txt", "utf8", () => {
   console.log("File Reading CB");
 });
 
-setTimeout(() => console.log("Timer expired"), 0);
+process.nextTick(() => console.log("process.nextTick2"));
 
-process.nextTick(() => console.log("process.nextTick"));
+
+setTimeout(() => console.log("Timer expired"), 1000);
+
+process.nextTick(() => console.log("process.nextTick3"));
+process.nextTick(() => console.log("process.nextTick4"));
+
+Promise.resolve().then(() => console.log("Promise1"));
+
+
 
 function printA() {
   console.log("a=", a);
@@ -21,3 +31,5 @@ function printA() {
 printA();
 
 console.log("Last line of the file.");
+
+Promise.resolve().then(() => console.log("Promise3"));
